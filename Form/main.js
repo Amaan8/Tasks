ff466040c146c7064003b38f49a4fef431e18e52
Amaan8 <amaanmas1@gmail.com>
2022-11-08 14:58:41 +0530

    scalable

--- Form/main.js
+++ Form/main.js
@@ -2,6 +2,7 @@ let myForm = document.getElementById('my-form');
 let nameInput = document.getElementById('name');
 let emailInput = document.getElementById('email');
 let msg = document.getElementById('msg');
+let list = document.getElementById('users');
 myForm.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
@@ -17,7 +18,14 @@ function onSubmit(e) {
             email: emailInput.value
         }
         let user = JSON.stringify(userDetails);
-        localStorage.setItem('userDetails', user);
+        localStorage.setItem('user_' + emailInput.value, user);
+
+        let li = document.createElement('li');
+        li.className = 'item';
+        let obj = JSON.parse(localStorage.getItem('user_' + emailInput.value));
+        let text = document.createTextNode(obj.name + ' : ' + obj.email);
+        li.appendChild(text);
+        list.appendChild(li);
 
         nameInput.value = '';
         emailInput.value = '';
