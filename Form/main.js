308d9906fdf5ca5c06bdde4f068c59717f5b30a2
Amaan8 <amaanmas1@gmail.com>
2022-12-19 22:16:35 +0530

    GET

--- Form/main.js
+++ Form/main.js
@@ -5,12 +5,15 @@ let msg = document.getElementById('msg');
 let list = document.getElementById('users');
 myForm.addEventListener('submit', onSubmit);
 
-// document.addEventListener('DOMContentLoaded', (event) => {
-//     Object.keys(localStorage).forEach((key) => {
-//         let obj = JSON.parse(localStorage.getItem(key));
-//         addItem(key, obj);
-//     })
-// })
+document.addEventListener('DOMContentLoaded', (event) => {
+    axios.get("https://crudcrud.com/api/4813370fb394426ebe50e70fa6f9e466/appointmentData")
+        .then((response) => {
+            for (let i = 0; i < response.data.length; i++) {
+                addItem(response.data[i]);
+            }
+        })
+        .catch(err => console.log(err));
+})
 
 function onSubmit(e) {
     e.preventDefault();
@@ -24,25 +27,12 @@ function onSubmit(e) {
             name: nameInput.value,
             email: emailInput.value
         }
-        // let user = JSON.stringify(userDetails);
-        // localStorage.setItem(user, user);
 
         axios.post("https://crudcrud.com/api/4813370fb394426ebe50e70fa6f9e466/appointmentData", userDetails)
             .then((response) => {
-                console.log(response.data);
                 addItem(response.data);
             })
             .catch(err => console.log(err));
-
-        // if (localStorage.getItem(user) !== null) {
-        //     let removeItem = document.getElementById(user);
-        //     if (removeItem) {
-        //         list.removeChild(removeItem);
-        //     }
-        // }
-
-        // let obj = JSON.parse(localStorage.getItem(user));
-        // addItem(user, obj);
 
         nameInput.value = '';
         emailInput.value = '';
@@ -59,13 +49,11 @@ function addItem(user) {
 }
 
 function deleteUser(user) {
-    // localStorage.removeItem(user);
     let removeItem = document.getElementById(user);
     list.removeChild(removeItem);
 }
 
 function editUser(user) {
-    // let obj = JSON.parse(localStorage.getItem(user));
     nameInput.value = user.name;
     emailInput.value = user.email;
     deleteUser(user);
