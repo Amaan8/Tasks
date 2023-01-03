44cebe147d82eed953241b09e217511e16016c39
Amaan8 <amaanmas1@gmail.com>
2023-01-03 19:40:40 +0530

    Added style

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,10 +1,23 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
+  const expenseDate = new Date(2023, 0, 3);
+  const expenseTitle = "Movies";
+  const expenseAmount = 200;
+  const locationOfExpenditure = "Delhi";
+
   return (
     <div>
       <h2>Expense Items</h2>
-      <p>Food Rs 10</p>
-      <p>Petrol Rs 100</p>
-      <p>Movies Rs 200</p>
+      <div className="expense-item">
+        <div>
+          {expenseDate.toISOString()} - {locationOfExpenditure}
+        </div>
+        <div className="expense-item__description">
+          <h2>{expenseTitle}</h2>
+          <div className="expense-item__price">Rs {expenseAmount}</div>
+        </div>
+      </div>
     </div>
   );
 }
