dc3a2c3afc106d0a460e9679a82e84dca508b906
Amaan8 <amaanmas1@gmail.com>
2023-01-05 18:26:36 +0530

    Dividing into more components

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -38,25 +38,25 @@ function App() {
         amount={expenses[0].amount}
         date={expenses[0].date}
         location={locationOfExpenditure}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
         location={locationOfExpenditure}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
         location={locationOfExpenditure}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
         location={locationOfExpenditure}
-      ></ExpenseItem>
+      />
     </div>
   );
 }
