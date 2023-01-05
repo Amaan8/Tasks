d7a5482d0ea499bde3eaa92012bbff339164cd14
Amaan8 <amaanmas1@gmail.com>
2023-01-05 22:28:54 +0530

    File Structure

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,6 +1,6 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -28,37 +28,12 @@ function App() {
     },
   ];
 
-  const locationOfExpenditure = "Delhi";
-
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-        location={locationOfExpenditure}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-        location={locationOfExpenditure}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-        location={locationOfExpenditure}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-        location={locationOfExpenditure}
-      />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
