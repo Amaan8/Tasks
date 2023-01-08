b23e1bce103e0b2dccb7c6d74e91a9d0a2df2782
Amaan8 <amaanmas1@gmail.com>
2023-01-08 19:22:42 +0530

    Added Event Listeners

--- React Course/src/components/Expenses/ExpenseItem.js
+++ React Course/src/components/Expenses/ExpenseItem.js
@@ -4,10 +4,21 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("Clicked!");
+  };
+
+  const deleteExpense = (e) => {
+    let item = e.target.parentElement;
+    item.remove();
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails title={props.title} amount={props.amount} />
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={deleteExpense}>Delete Expense</button>
     </Card>
   );
 };
