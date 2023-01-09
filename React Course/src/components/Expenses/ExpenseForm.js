c60f5b9755f1bce55cded94a8282e8842903cf94
Amaan8 <amaanmas1@gmail.com>
2023-01-09 19:07:44 +0530

    Added states

--- React Course/src/components/Expenses/ExpenseForm.js
+++ React Course/src/components/Expenses/ExpenseForm.js
@@ -1,8 +1,24 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const changeHandler = (e) => {
-    console.log(e.target.value);
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titleChange = (e) => {
+    setEnteredTitle(e.target.value);
+    console.log(enteredTitle);
+  };
+
+  const amountChange = (e) => {
+    setEnteredAmount(e.target.value);
+    console.log(enteredAmount);
+  };
+
+  const dateChange = (e) => {
+    setEnteredDate(e.target.value);
+    console.log(enteredDate);
   };
 
   return (
@@ -10,11 +26,11 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={changeHandler} />
+          <input type="text" onChange={titleChange} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="1" onChange={changeHandler} />
+          <input type="number" min="1" onChange={amountChange} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
@@ -22,7 +38,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
-            onChange={changeHandler}
+            onChange={dateChange}
           />
         </div>
       </div>
