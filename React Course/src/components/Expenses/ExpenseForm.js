df29879c461aa6075dfab92b61c3800215767951
Amaan <amaanmas1@gmail.com>
2023-01-13 20:05:38 +0530

    Added cancel button

--- React Course/src/components/Expenses/ExpenseForm.js
+++ React Course/src/components/Expenses/ExpenseForm.js
@@ -5,6 +5,7 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+  const [addExpense, setAddExpense] = useState(false);
 
   const titleChange = (e) => {
     setEnteredTitle(e.target.value);
@@ -33,39 +34,54 @@ const ExpenseForm = (props) => {
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
+
+    setAddExpense(false);
   };
 
+  const newExpense = () => {
+    setAddExpense(true);
+  }
+
+  const cancelAdding = () => {
+    setAddExpense(false);
+  }
+
   return (
-    <form className="new-expense" onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input type="text" value={enteredTitle} onChange={titleChange} />
+    <div className="new-expense">
+      {!addExpense && <button onClick={newExpense}>Add New Expense</button>}
+      {addExpense && 
+      <form onSubmit={submitHandler}>
+        <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <label>Title</label>
+            <input type="text" value={enteredTitle} onChange={titleChange} />
+          </div>
+          <div className="new-expense__control">
+            <label>Amount</label>
+            <input
+              type="number"
+              min="1"
+              value={enteredAmount}
+              onChange={amountChange}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Date</label>
+            <input
+              type="date"
+              min="2019-01-01"
+              max="2022-12-31"
+              value={enteredDate}
+              onChange={dateChange}
+            />
+          </div>
         </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="1"
-            value={enteredAmount}
-            onChange={amountChange}
-          />
+        <div className="new-expense__actions">
+          <button type="button" onClick={cancelAdding}>Cancel</button>
+          <button type="submit">Add Expense</button>
         </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2019-01-01"
-            max="2022-12-31"
-            value={enteredDate}
-            onChange={dateChange}
-          />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-      </div>
-    </form>
+      </form>}
+    </div>
   );
 };
 
