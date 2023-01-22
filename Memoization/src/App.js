7e8469a0fd8a61a6ca2a943cd4095cef141c86df
Amaan <amaanmas1@gmail.com>
2023-01-22 17:30:06 +0530

    Memoization

---
+++ Memoization/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState, useCallback, useMemo } from 'react';
+
+import './App.css';
+import DemoList from './components/Demo/DemoList';
+import Button from './components/UI/Button/Button';
+
+function App() {
+  const [listTitle, setListTitle] = useState('My List');
+  const [order, setOrder] = useState(false);
+
+  const changeTitleHandler = useCallback(() => {
+    setListTitle('New Title');
+  }, []);
+
+  const changeOrder = () => {
+    setOrder(prevOrder => !prevOrder);
+  }
+
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
+
+  return (
+    <div className="app">
+      <DemoList title={listTitle} items={listItems} order={order} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
+      <Button onClick={changeOrder}>{order ? 'Change to ascending order' : 'Change to descending order'}</Button>
+    </div>
+  );
+}
+
+export default App;
