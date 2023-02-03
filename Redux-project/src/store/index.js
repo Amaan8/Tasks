078153da849beb9a3b63623cb185f50799949511
Amaan <amaanmas1@gmail.com>
2023-02-03 17:45:06 +0530

    Added more action types

--- Redux-project/src/store/index.js
+++ Redux-project/src/store/index.js
@@ -11,6 +11,16 @@ const counterReducer = (state = { counter: 0 }, action) => {
       counter: state.counter - 1,
     };
   }
+  if (action.type === "incrementBy2") {
+    return {
+      counter: state.counter + 2,
+    };
+  }
+  if (action.type === "decrementBy2") {
+    return {
+      counter: state.counter - 2,
+    };
+  }
   return state;
 };
 
